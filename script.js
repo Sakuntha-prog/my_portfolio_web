05b3c9bfba21cd2cbd0dc33880c4d815e0376992
Sakuntha-prog <sakunthasankalpa67@gmail.com>
2025-08-31 16:59:32 +0530

    sample section aimation and navigation

---
+++ script.js
@@ -0,0 +1,14 @@
+    // Fade-in animation on scroll
+    const sections = document.querySelectorAll("section");
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("active");
+        }
+      });
+    }, { threshold: 0.3 });
+
+    sections.forEach(section => {
+      observer.observe(section);
+    });
